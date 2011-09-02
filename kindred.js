516f844838c17aeedc93e3e35ea6eb830427ce0b
Nathaniel Smith <nathanielksmith@gmail.com>
2011-09-01 21:27:09 -0400

    clean up, rename some vars

--- kindred.js
+++ kindred.js
@@ -1,34 +1,37 @@
 #!/usr/bin/env node
 var fs = require('fs');
 var path = require('path');
+
 var mandir = require('./mandir.js');
+var markdown = require('markdown');
 
-// Usage: kindred.js published_dir target_dir
+// Usage: kindred.js template_file publish_dir target_dir
 
-var published = process.argv[1];
-var target = process.argv[2];
-
-if (!published) { process.exit("Need a published directory."); }
-
-if (!target) { process.exit("Need a target directory to publish index.html to"); }
+var template_file = process.argv[1];
+var publish_dir = process.argv[2];
+var target = process.argv[3];
+if (!template_file) { process.exit("Need a template file path."); }
+if (!publish_dir) { process.exit("Need a publish_dir directory."); }
+if (!target_dir) { process.exit("Need a target directory to publish index.html to"); }
 
 function tick() {
-  // Collect posts from publish_dir and fill up an array to pass to publish()
+  // Collect posts from publish_dir and fill up an array to render as
+  // target_dir/index.html with template_file
   var files_in = 0;
   var files_out = 0;
   var posts = [];
-  fs.readDir(published, function(err, files) {
+  fs.readDir(publish_dir, function(err, files) {
     files.forEach(function(x) {
       files_in++;
       // TODO support _img and friends
       if (!x.match(/_txt$/)) { return; }
-      fs.readFile(path.join(published, x), 'utf8', function(err, data) {
+      fs.readFile(path.join(publish_dir, x), 'utf8', function(err, data) {
         if (err) { console.log("Err reading "+x); files_out++; return; }
         var publish_date = data.match(/_\d\d\d\d-\d\d-\d\d \d\d:\d\d_\s*$/);
         if (!publish_date) { console.log("No publish date for "+x); files_out++; return;) }
         posts.push({
           id: x,
-          content: md.Markdown(data),
+          content: markdown.toHTML(data),
           publish_date: publish_date.toString()
         });
         files_out++;
@@ -39,7 +42,7 @@ function tick() {
             else return 0;
           });
           template.render(template_path, {posts:posts}, function(err, html) {
-            fs.writeFile(path.join(publish_dir, 'index.html'), html, function(err) {
+            fs.writeFile(path.join(target_dir, 'index.html'), html, function(err) {
               if (err) { console.log("error writing to "+publish_dir+" index.html"); }
             });
           });
